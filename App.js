fe59d12c4338409271ef11ed32837659e7ac57ab
sonali-01 <sonali.sk@robosoftin.com>
2020-10-26 22:30:26 +0530

    app.js is added

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Game from './TicTacTo';
+
+import React from 'react';
+
+function App(){
+    return (
+        <div className="App">
+            <Game />
+        </div>
+    )
+}
+
+export default App;
